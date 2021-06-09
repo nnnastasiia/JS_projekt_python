68e3dba611a1b1c1fdf3f1d2865c22c4840528fb
nnnastasiia <nastka.mishchenko@gmail.com>
2021-06-09 17:28:24 +0200

    main

--- main.py
+++ main.py
@@ -4,8 +4,14 @@ from Parkomat_app.Parkomat import *
 from parkomat_money.przechowywaczMonet import PrzechowywaczMonet
 
 
+def main():
+    '''
+    Funkcja główna która wykonuje i uruchamia cały program
+    '''
+    root=Tk()
+    przechowywaczMonet=PrzechowywaczMonet()
+    my_window=Parkomat(root, przechowywaczMonet)
+    my_window.startWindow()
 
-root=Tk()
-przechowywaczMonet=PrzechowywaczMonet()
-my_window=Parkomat(root, przechowywaczMonet)
-my_window.startWindow()
+if __name__ == '__main__':
+    main() 
